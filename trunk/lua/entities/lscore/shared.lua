106370010f5283d1ff8527d62062e73fb3932fbe
space-explorer@hotmail.com <space-explorer@hotmail.com@9cec207b-48dd-5834-3964-10fe9034192e>
2011-01-30 23:28:24 +0000

    Update to new version :D

---
+++ trunk/lua/entities/lscore/shared.lua
@@ -0,0 +1,19 @@
+
+ENT.Type = "anim"
+ENT.Base = "base_rd3_entity"
+ENT.PrintName = "LS Core"
+ENT.Author = "CmdrMatthew"
+ENT.Purpose = "To Test"
+ENT.Instructions = "Eat up!" 
+ENT.Category = "Environments"
+
+ENT.Spawnable = true
+
+
+list.Set( "LSEntOverlayText" , "lscore", {HasOOO = true, resnames ={ "oxygen", "energy", "water", "nitrogen"} } )
+
+/*if CLIENT then
+	function ENT:Draw()
+	
+	end
+end*/
