ca69ca9165011af1269f3157a5958da2ede738bb
space-explorer <space-explorer@hotmail.com>
2011-02-27 04:06:00 +0000

    Added new spawn property for planets to stop random events from happening there.

--- lua/environments/events/sv_events.lua
+++ lua/environments/events/sv_events.lua
@@ -7,10 +7,20 @@
 local events = {}
 events["asteroidstorm"] = function()
 	local planet = table.Random(environments)
-	local roids = ents.Create("event_asteroid_storm")
-	roids:SetPos(planet.position + Vector(0, 0, planet.radius + 2000))
-	roids:Spawn()
-	roids:Start(planet.radius)
+	if planet.spawn == "1" then
+		planet = table.Random(environments)
+		if planet.spawn == "0" then
+			local roids = ents.Create("event_asteroid_storm")
+			roids:SetPos(planet.position + Vector(0, 0, planet.radius + 2000))
+			roids:Spawn()
+			roids:Start(planet.radius)
+		end
+	else
+		local roids = ents.Create("event_asteroid_storm")
+		roids:SetPos(planet.position + Vector(0, 0, planet.radius + 2000))
+		roids:Spawn()
+		roids:Start(planet.radius)
+	end
 end
 
 local function FireEvent(ply,cmd,args)
@@ -20,6 +30,7 @@ end
 concommand.Add("env_fire_event", FireEvent)
 
 local function EventChecker()
+	
 	if math.random(1,5) == 3 then
 		print("Event Running")
 		//call the function to run the event
