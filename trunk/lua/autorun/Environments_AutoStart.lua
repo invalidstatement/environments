46d9e5ca0684393d6b06447120d89631bf7f9ee3
space-explorer@hotmail.com <space-explorer@hotmail.com@9cec207b-48dd-5834-3964-10fe9034192e>
2011-03-02 22:28:24 +0000

    HUD improvements for several resolutions

--- trunk/lua/autorun/Environments_AutoStart.lua
+++ trunk/lua/autorun/Environments_AutoStart.lua
@@ -8,7 +8,7 @@
 //3. Make it so you can refill your suit without LS3
 //4. HUD customizations
 Environments = {}
-Environments.Version = 77
+Environments.Version = 78
 Environments.FileVersion = 2
 local onlineversion
 
--- trunk/lua/environments/spacesuits/cl_suit.lua
+++ trunk/lua/environments/spacesuits/cl_suit.lua
@@ -8,3 +8,30 @@ local function DrawPlayer( pl )
 end
 hook.Add( "ShouldDrawLocalPlayer", "PlayerSetDraw", DrawPlayer)
 
+//Player bones are wierd, they are based on thier scale times their parent's scale so 0.5 the child x0.5 for the parent
+//the child would be scaled 0.25
+/*function MakeSuitReady()
+	for k,v in pairs(player.GetAll()) do
+		function v:BuildBonePositions( NumBones, NumPhysBones )
+			local realboneid = self:LookupBone("ValveBiped.Bip01_Head1")
+			local pos, angles = self:GetBonePosition(realboneid)
+			for i=0, NumBones do
+				BoneScale( self, i )
+			end
+			local matBone = self:GetBoneMatrix( realboneid )
+			matBone:Scale( Vector( 12, 12, 12 ) )
+			self:SetBoneMatrix( realboneid, matBone )
+			self:SetBonePosition(realboneid, pos, angles)
+		end
+	end
+end
+timer.Create("suitsstuff", 10, 0, MakeSuitReady)
+
+function BoneScale( self, realboneid )
+	if self:GetBoneName(realboneid) == "ValveBiped.Bip01_Head1" then return end
+	local matBone = self:GetBoneMatrix( realboneid )
+	if matBone then
+		matBone:Scale( Vector( 0.75, 0.75, 0.75 ) )
+		self:SetBoneMatrix( realboneid, matBone )
+	end
+end*/
