ca22a803cfcb8b5d1d92a283de18af35c1c619fa
space-explorer@hotmail.com <space-explorer@hotmail.com@9cec207b-48dd-5834-3964-10fe9034192e>
2011-10-20 22:40:19 +0000

    added terraformer, added admin only generator, added new RD function ent:GetStorageLeft(resource), added new entity property ENT.AdminOnly to make ents that can only be spawned by admins with the tools

--- trunk/Environments Core/lua/autorun/Environments_AutoStart.lua
+++ trunk/Environments Core/lua/autorun/Environments_AutoStart.lua
@@ -8,7 +8,7 @@ if not Environments then
 end
 
 Environments.Hooks = {}
-Environments.Version = 139
+Environments.Version = 140
 Environments.CurrentVersion = 0 --for update checking
 Environments.FileVersion = 8
 
