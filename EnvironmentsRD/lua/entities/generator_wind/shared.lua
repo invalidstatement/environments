d6852fdb80edabbffe3fda2e7c1a3b375f961199
kingludsoe <kingludsoe@9cec207b-48dd-5834-3964-10fe9034192e>
2013-07-27 19:13:51 +0000

    Removed the multiplier slider from the devices multipliers don't work on.

--- EnvironmentsRD/lua/entities/generator_wind/shared.lua
+++ EnvironmentsRD/lua/entities/generator_wind/shared.lua
@@ -4,3 +4,28 @@ ENT.PrintName 	= "Wind Generator"
 ENT.AutomaticFrameAdvance = true  
 
 list.Set( "LSEntOverlayText" , "generator_wind", {HasOOO = true, genresnames={"energy"}} )
+
+if(SERVER)then
+	
+	local T = {} --Create a empty Table
+	
+	T.Power = function(Device,ply,Data)
+		Device:SetActive( nil, ply )
+	end
+	
+	ENT.Panel=T --Set our panel functions to the table.
+	
+else 
+	function ENT:PanelFunc(um,e,entID)
+	
+		e.Functions={}
+		
+		e.DevicePanel = [[
+		@<Button>Toggle Power</Button><N>PowerButton</N><Func>Power</Func>
+		]]
+
+		e.Functions.Power = function()
+			RunConsoleCommand( "envsendpcommand",entID,"Power")
+		end
+	end
+end
